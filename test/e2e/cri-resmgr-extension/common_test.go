14e068c8eee9e4d111ddc2eee2f2e8bd8086c12b
Damian Darczuk <damian.darczuk@intel.com>
2023-03-09 19:25:37 +0100

    More time for tests

--- test/e2e/cri-resmgr-extension/common_test.go
+++ test/e2e/cri-resmgr-extension/common_test.go
@@ -30,7 +30,7 @@ import (
 var (
 	// commonLabel      = "cri-rm"
 	backgroundCtx   = context.Background()
-	fiveteenMinutes = 30 * time.Minute
+	fiveteenMinutes = 60 * time.Minute
 	ExtensionType   = "cri-resmgr-extension"
 
 	// Those options can be overridden with arguments like: -verbose, -disable-dump, -existing-shoot-name -kubecfg -project-namespace
